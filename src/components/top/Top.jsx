a031867049d7ee50475d42fab8100433da73c718
Colais <nicholasbalby@hotmail.com>
2021-04-01 21:45:13 -0300

    Configuraçao base, SASS, diretorios, package.json, components e dependencias.

---
+++ src/components/top/Top.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Top = () => (
+    <div className="top">
+        Top
+        <div className='top__coin'>
+            Coin Opa
+        </div>
+    </div>
+)
+
+export default Top;
