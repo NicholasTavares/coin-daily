b32ce85713a374ed8305322ee3bf29b3bca46831
Colais <nicholasbalby@hotmail.com>
2021-04-07 18:11:41 -0300

    Detalhamento de moeda concluido.

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -12,18 +12,35 @@ const Top = () => {
                 <div className="container-top__head-background">
                 </div>
 
-                <div className="container-top__porcentagem">
-                    <h1 className='container-top__head-background__price'>R$ 12,287.03</h1>
-                    <div className="container-top__head-background__porcentagem__24-h">
-                        <h1>&darr; 3.83%</h1>
+                <div className="container-top__price-container">
+                    <h1 className='container-top__price-container__price'>R$ 12,287.03</h1>
+
+                    <div className='container-top__price-container__percentage-container'>
+                        <span className='container-top__price-container__percentage-time'>
+                            24h %
+                        </span>
+                        <span className="container-top__price-container__percentage-price">
+                            &darr; 3.83%
+                        </span>
+
                     </div>
+
+                    <div className='container-top__price-container__percentage-container'>
+                        <span className='container-top__price-container__percentage-time'>
+                            7d %
+                        </span>
+                        <span className="container-top__price-container__percentage-price">
+                            &uarr; 14.45%
+                        </span>
+                    </div>
+
                 </div>
 
                 <div className="container-top__head">
                     <div className="container-top__data">
                         <h1 className="container-top__top-level">2</h1>
                         <h2 className="container-top__title">Ethereum</h2>
-                        <span className="container-top__price">R$ 12,287.03</span>
+                        <span className="container-top__price-title">R$ 12,287.03</span>
                     </div>
                 </div>
 
@@ -34,18 +51,35 @@ const Top = () => {
                 <div className="container-top__head-background">
                 </div>
 
-                <div className="container-top__porcentagem">
-                    <h1 className='container-top__head-background__price'>R$ 339,788.75</h1>
-                    <div className="container-top__head-background__porcentagem__24-h">
-                        <h1>&darr; 3.83%</h1>
+                <div className="container-top__price-container">
+                    <h1 className='container-top__price-container__price'>R$ 339,788.75</h1>
+
+                    <div className='container-top__price-container__percentage-container'>
+                        <span className='container-top__price-container__percentage-time'>
+                            24h %
+                        </span>
+                        <span className="container-top__price-container__percentage-price">
+                            &darr; 9.11%
+                        </span>
+
                     </div>
+
+                    <div className='container-top__price-container__percentage-container'>
+                        <span className='container-top__price-container__percentage-time'>
+                            7d %
+                        </span>
+                        <span className="container-top__price-container__percentage-price">
+                            &uarr; 8.09%
+                        </span>
+                    </div>
+
                 </div>
 
                 <div className="container-top__head">
                     <div className="container-top__data">
                         <h1 className="container-top__top-level">1</h1>
                         <h2 className="container-top__title">Bitcoin</h2>
-                        <span className="container-top__price">R$ 339,788.75</span>
+                        <span className="container-top__price-title">R$ 339,788.75</span>
                     </div>
                 </div>
 
@@ -56,18 +90,35 @@ const Top = () => {
                 <div className="container-top__head-background">
                 </div>
 
-                <div className="container-top__porcentagem">
-                    <h1 className='container-top__head-background__price'>R$ 1,931.91</h1>
-                    <div className="container-top__head-background__porcentagem__24-h">
-                        <h1>&darr; 3.83%</h1>
+                <div className="container-top__price-container">
+                    <h1 className='container-top__price-container__price'>R$ 1,931.91</h1>
+
+                    <div className='container-top__price-container__percentage-container'>
+                        <span className='container-top__price-container__percentage-time'>
+                            24h %
+                        </span>
+                        <span className="container-top__price-container__percentage-price">
+                            &darr; 1.83%
+                        </span>
+
                     </div>
+
+                    <div className='container-top__price-container__percentage-container'>
+                        <span className='container-top__price-container__percentage-time'>
+                            7d %
+                        </span>
+                        <span className="container-top__price-container__percentage-price">
+                            &uarr; 42.59%
+                        </span>
+                    </div>
+
                 </div>
 
                 <div className="container-top__head">
                     <div className="container-top__data">
                         <h1 className="container-top__top-level">3</h1>
                         <h2 className="container-top__title">Coin Binance</h2>
-                        <span className="container-top__price">R$ 1,931.91</span>
+                        <span className="container-top__price-title">R$ 1,931.91</span>
                     </div>
                 </div>
 
