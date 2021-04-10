7405e1e7518dbe763c112cdc3d321a7fa4815f50
Colais <nicholasbalby@hotmail.com>
2021-04-10 20:17:09 -0300

    Slide funcionando atraves de evento de click.

--- src/components/coin/book/book.jsx
+++ src/components/coin/book/book.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { traverseTwoPhase } from 'react-dom/test-utils';
 import { FiChevronDown } from 'react-icons/fi';
 
 const Book = () => {
 
+    const [slide, setSlide] = useState(false)
+
     return (
         <div className="container-top__price-container__container-book">
-            <span className="container-top__price-container__container-book__arrow">
+            <span className={`container-top__price-container__container-book__arrow ${slide ? 'arrowRotate' : ''}`} onClick={() => setSlide(!slide)}>
                 <FiChevronDown />
             </span>
 
-            <div className="container-top__price-container__container-book__book">
+            <div className={`container-top__price-container__container-book__book ${slide ? 'slide' : ''}`}>
 
             </div>
         </div>
