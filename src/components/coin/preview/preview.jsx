967816c05f7394a85111e7f3b68c100c200e2d2f
Colais <nicholasbalby@hotmail.com>
2021-04-11 21:53:11 -0300

    Corrigindo bordas de status e tambem formatacao de numeros.

--- src/components/coin/preview/preview.jsx
+++ src/components/coin/preview/preview.jsx
@@ -7,7 +7,7 @@ const Preview = ({ coin_name, price, logo }) => {
             <div className="container-top__preview">
                 <h1 className="container-top__logo">{logo}</h1>
                 <h2 className="container-top__title">{coin_name}</h2>
-                <span className="container-top__price-title">R$ {price ? parseFloat(price.buy).toFixed(3) : 'Loading'}</span>
+                <span className="container-top__price-title">R$ {price ? parseFloat(price.buy).toLocaleString('pt-BR') : 'Loading'}</span>
             </div>
         </div>
     )
--- src/components/coin/price/price.jsx
+++ src/components/coin/price/price.jsx
@@ -5,14 +5,14 @@ const Price = ({ price, coin_name }) => {
     return (
         <div className="container-top__price-container">
             <h1 className="container-top__price-container__principal-title">{coin_name}</h1>
-            <h2 className='container-top__price-container__price'>{price ? `R$ ${parseFloat(price.buy).toFixed(3)}` : 'Loading'}</h2>
+            <h2 className='container-top__price-container__price'>{price ? `R$ ${parseFloat(price.buy).toLocaleString('pt-BR')}` : 'Loading'}</h2>
 
             <div className='container-top__price-container__percentage-container'>
                 <span className='container-top__price-container__percentage-time'>
                     MAX
                 </span>
                 <span className="container-top__price-container__percentage-price">
-                    {price ? <span style={{ color: 'white' }}>{(price.high * 1).toFixed(2)}</span> : ''}
+                    {price ? <span style={{ color: 'white' }}>R$ {(price.high * 1).toLocaleString('pt-BR', { maximumFractionDigits: 2 })}</span> : 'Loading'}
                 </span>
             </div>
 
@@ -21,7 +21,7 @@ const Price = ({ price, coin_name }) => {
                     MIN
                 </span>
                 <span className="container-top__price-container__percentage-price">
-                    {price ? <span style={{ color: 'white' }}>{(price.low * 1).toFixed(2)}</span> : ''}
+                    {price ? <span style={{ color: 'white' }}>R$ {(price.low * 1).toLocaleString('pt-BR')}</span> : 'Loading'}
                 </span>
             </div>
 
